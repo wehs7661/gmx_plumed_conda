e7b7331bc9ae86edc62e0b70655c03fca5c62c73
wehs7661 <wehs7661@colorado.edu>
2024-09-02 02:16:10 -0400

    Updated build.sh and meta.yaaml

--- build.sh
+++ build.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# Download and extract GROMACS
-wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-2023.5.tar.gz
-tar -zxvf gromacs-2023.5.tar.gz
-cd gromacs-2023.5
-
-# Create build directory
+# Install GROMACS 2023.5 (automatically downloaded as instructed by meta.yaml)
+plumed patch -p -e gromacs-2023.5
 mkdir build && cd build
-
-# Configure the build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DGMX_SIMD=AVX2_256 \
     -DGMX_MPI=ON \
     -DGMX_BUILD_OWN_FFTW=ON
-
-# Patch with PLUMED
-plumed patch -p -e gromacs-2023.5
-
-# Compile and install
 make -j$(nproc)
 make install
-
-# Clean up
-cd ..
+cd ../
 rm -rf gromacs-2023.5
